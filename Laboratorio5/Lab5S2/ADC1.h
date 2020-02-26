8fa305317e1ca3ba8f9338a418317e42463b1b8d
Nancy Alejandra Mazariegos Ortiz <45055985+Imphanda@users.noreply.github.com>
2020-02-26 03:16:18 -0600

    Add files via upload

---
+++ Laboratorio5/Lab5S2/ADC1.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ADC1.h
+ * Author: Nancy Mazariegos
+ *
+ * Created on February 25, 2020, 11:30 AM
+ */
+
+#ifndef ADC1_H
+#define	ADC1_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADC1_H */
+
---
+++ Laboratorio5/Lab5S2/I2CS2.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   i2cs2.h
+ * Author: Nancy Mazariegos
+ *
+ * Created on February 25, 2020, 11:56 AM
+ */
+
+#ifndef I2CS2_H
+#define	I2CS2_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* I2CS2_H */
+
