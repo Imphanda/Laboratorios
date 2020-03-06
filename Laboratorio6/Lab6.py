e748c475dbce4e64a29e9e5469997c20b0627c9b
Nancy Alejandra Mazariegos Ortiz <45055985+Imphanda@users.noreply.github.com>
2020-03-06 11:40:36 -0600

    Add files via upload

---
+++ Laboratorio6/Lab6.py
@@ -0,0 +1,17 @@
+#Nancy Mazariegos
+#Laboratorio 6 RaspberryPi
+from Adafruit_IO import *
+import random
+ADAFRUIT_IO_USERNAME = "NancyM20"
+ADAFRUIT_IO_KEY = "aio_HdGw57ohvqLy3IzNcBmxUujTe3Xy"
+aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
+
+sensor_1=random.randint(0,100)
+sensor_2=random.randint(0,100)
+sensor_3=random.randint(0,100)
+sensor_4=random.randint(0,100)
+
+aio.send("sensor1", sensor_1)
+aio.send("sensor2", sensor_2)
+aio.send("sensor3", sensor_3)
+aio.send("sensor4", sensor_4)
